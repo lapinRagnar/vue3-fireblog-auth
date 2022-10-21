fbe1c1d125ac18b062e7c4263622daa616f23020
lapinRagnar <lapinragnar@gmail.com>
2022-10-21 10:59:29 +0200

    1 - installation - vue2editor - quill -quill-image-resize-module vue-svg-loader

--- README.md
+++ README.md
@@ -17,3 +17,20 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### documentation et tous les technos qu'on utilise dans ce projet
+
+=> vue2editor:
+https://www.vue2editor.com/guide.html#installation
+
+=> quill :  modern rich text editor built for compatibility and extensibility
+https://github.com/quilljs/quill
+
+=> quill-image-resize-module:
+https://github.com/kensnyder/quill-image-resize-module
+
+=> vue-svg-loader:
+https://github.com/damianstasik/vue-svg-loader
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>Page app</div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
+  @import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Quicksand", sans-serif;
   }
-}
+
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  .container {
+    max-width: 1440px;
+    margin: 0 auto;
+  }
+
+  .link {
+    cursor: pointer;
+    text-decoration: none;
+    text-transform: uppercase;
+    color: black;
+  }
+
+  .link-light {
+    color: #fff;
+  }
+
 </style>
