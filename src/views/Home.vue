e95ff95343f1e759a91d7e054e85a6b6017c9b3d
lapinRagnar <lapinragnar@gmail.com>
2022-10-24 17:43:36 +0200

    Home - BlogPost card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="home">
+    
     <BlogPost :post="welcomeScreen" />
-    <BlogPost :post="post" v-for="(post, index) in sampleBlogPosts" :key="index"> 
-      
-    </BlogPost> 
+    
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogPosts" :key="index" /> 
+    
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCard :post="post" v-for="(post, index) in sampleBlogCards" :key="index" />
+        </div>
+      </div>
+    </div>
+     
   </div>
 </template>
 
@@ -11,12 +21,14 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import BlogPost from '@/components/BlogPost.vue';
+import BlogCard from '@/components/BlogCard.vue';
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld,
-    BlogPost
+    BlogPost,
+    BlogCard
 },
   data(){
     return {
@@ -45,9 +57,37 @@ export default {
           blogHTML: 'ceci est mon super contenu1',
           blogCoverPhoto: 'designed-for-everyone'
         },
+      ],
+
+      sampleBlogCards: [
+        { blogTitle: 'BlogCard #1', blogCoverPhoto: 'stock-1', blogDate: 'Jan 1, 2022'},
+        { blogTitle: 'BlogCard #2', blogCoverPhoto: 'stock-2', blogDate: 'Jan 2, 2022'},
+        { blogTitle: 'BlogCard #3', blogCoverPhoto: 'stock-3', blogDate: 'Jan 3, 2022'},
+        { blogTitle: 'BlogCard #4', blogCoverPhoto: 'stock-4', blogDate: 'Jan 4, 2022'},
+        { blogTitle: 'BlogCard #5', blogCoverPhoto: 'stock-5', blogDate: 'Jan 5, 2022'},
+        { blogTitle: 'BlogCard #6', blogCoverPhoto: 'stock-6', blogDate: 'Jan 6, 2022'},
       ]
     
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .blog-card-wrap {
+
+    .container {
+
+      h3 {
+        font-weight: 300;
+        font-style: 28px;
+        margin-bottom: 32px;
+      }
+
+    }
+
+
+  }
+
+
+</style>
