95c48bdcd43e3301787feb0ee23be82b464854da
lapinRagnar <lapinragnar@gmail.com>
2022-10-26 18:02:54 +0200

    Editer le Profile Utilisateur -
    populer les infos du profile user dans le formulaire
    
    avec computed et le store
    
    - disabled le champ email parce qu'on ne veut pas le modifier
    
    - créer les fonctions de set() dans le store index
    
    - et creer une action pour envoyer les modifs dans le backend

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'vuex'
 
 import {app}  from '@/firebase/firebaseInit'
-import { getFirestore, doc, getDoc, collection, getDocs, query, where} from 'firebase/firestore'
+import { getFirestore, doc, getDoc, setDoc, collection, getDocs, query, where} from 'firebase/firestore'
 import { getAuth, createUserWithEmailAndPassword  } from "firebase/auth"
 
 
@@ -26,11 +26,15 @@ export default createStore({
     profileUserName: null,
     profileId: null,
     profileInitials: null,
+    profileDocId: null,
 
   },
+
   getters: {
   },
+
   mutations: {
+
     toggleEditPost(state, payload) {
       state.editPost = payload
       console.log('dans le store', state.editPost)
@@ -49,18 +53,36 @@ export default createStore({
       state.profileLastName = payload.lastName
       state.profileUserName = payload.username
       state.profileId = payload.id
+      state.profileDocId = payload.docId
 
-      // state.profileEmail = payload.data().email
-      // state.profileFirstName = payload.data().firstName
-      // state.profileLastName = payload.data().lastName
-      // state.profileUserName = payload.data().userName
     },
 
     setProfileInitials(state) {
-      state.profileInitials = state.profileFirstName.substr(0, 1).toUpperCase() + state.profileLastName.substr(0, 1).toUpperCase()
-    }
+      // state.profileInitials = 'AB'
+      console.log('aaaaaaa', state.profileFirstName)
+
+      if (state.profileFirstName == null ) {
+        state.profileInitials = 'AN'
+      } else {
+        state.profileInitials = state.profileFirstName.replace(/ /g, "").substr(0, 1).toUpperCase() + state.profileLastName.replace(/ /g, "").substr(0, 1).toUpperCase()
+      }
+    },
+
+    changeFistName(state, payload) {
+      state.profileFirstName = payload
+    },
+
+    changeLastName(state, payload) {
+      state.profileLastName = payload
+    },
+
+    changeUserName(state, payload) {
+      state.profileUserName = payload
+    },
 
   },
+
+
   actions: {
 
     async getCurrentUser({commit}) {
@@ -73,8 +95,7 @@ export default createStore({
       console.log('current user dans action - email', auth.currentUser.email);
       console.log('current user dans action - id', auth.currentUser.uid);
 
-      const q = query(collection(database, "users"), where("email", "==", auth.currentUser.email))
-      
+      const q = query(collection(database, "users"), where("email", "==", auth.currentUser.email))      
       console.log("chercher l'user qui a l'email = à le meme email que le current user", q)  
 
       const querySnapshot = await getDocs(q)
@@ -83,89 +104,50 @@ export default createStore({
 
 
       querySnapshot.forEach((doc) => {
+
+        console.log('je suis la')
+
         console.log('dans actions getCurrentUser', doc.id, " => ", doc.data())
         const dbResults = doc.data()
         console.log('id ', auth.currentUser.uid);
         dbResults['id'] = auth.currentUser.uid
+        dbResults['docId'] = doc.id
         console.log('ajouter id', dbResults)
         commit('setProfileInfo', dbResults)
+        commit('setProfileInitials')
       });
 
       commit('setProfileInitials')
 
-      // ------------------------------------
+    },
 
-      // const dbResults = [] 
-      // const querySnapshot = await getDocs(collection(database, "users"))      
-      // querySnapshot.forEach((doc) => {
-      //   // doc.data() is never undefined for query doc snapshots
-      //   console.log(doc.id, " => ", doc.data())
-      //   // const document = { [doc.id]: doc.data() }
-      //   const document = doc.data() 
-      //   dbResults.push(document)
-      // });
+    async updateUserSetting({ commit, state }) {
 
-      // console.log('dbResults', dbResults)
-      // commit('setProfileInfo', dbResults)
-      // commit('setProfileInitials')
+      const database = getFirestore(app)
+      
+      const docRef = doc(database, "users", state.profileDocId);
 
+      const data = {
+        firstName: state.profileFirstName,
+        lastName: state.profileLastName,
+        username: state.profileUserName,
+      }
 
-      // -------------------------------------
-      // const firebaseAuth = await getAuth(bdd)
-
-      // const dataBase = await db.collection('users').doc(app.auth().currentUser.uid)
-      // const dbResults = await dataBase.get()
-      // commit(commit('setProfileInfo', dbResults))
-      // commit('setProfileInitials')
-      // console.log('dbResults', dbResults)
-
-      // -------------------------------
-
-      // const dbResults = []
-
-      // try {
-
-      //   const docRef = collection(database, "users")
-      //   const docsSnap = await getDocs(docRef);
-  
-      //   console.log('docRef, docsSnap', docRef, docsSnap)
-
-      //   if(docsSnap.docs.length > 0) {
-
-      //     docsSnap.forEach(doc => {
-      //         console.log(doc.data());
-      //         dbResults.push(doc.data())
-      //     })
-
-      //     console.log('dbResults', dbResults)
-
-      //     // this.commit('setProfileInfo', docsSnap.docs )
-      //     // this.commit('setProfileInfo', docsSnap )
-  
-      //   }
-
-      // } catch (error) {
-      //   console.log(error);
-      // }
-
-      // -------------------------------------------
-
-      // commit('setProfileInfo', dbResults)
-      // commit('setProfileInitials')
-
-      // if (docsSnap) {
-      //   console.log("Document data:", docsSnap.data())
-      //   const dbResults = docsSnap.data()
-      //   commit('setProfileInfo', dbResults)
-      //   commit('setProfileInitials')
-      // } else {
-      //   // doc.data() will be undefined in this case
-      //   console.log("No such document!")
-      // }
-
+      setDoc(docRef, data)
+        .then(docRef => {
+            console.log("Entire Document has been updated successfully", docRef)
+            commit('setProfileInitials')
+        })
+        .catch(error => {
+            console.log(error);
+        })
+      
 
     }
+
   },
+
+
   modules: {
   }
 })
